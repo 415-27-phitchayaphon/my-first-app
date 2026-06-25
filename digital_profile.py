c7f868f549ce639e8d622c573ca6d7da1621925d
415-27-phitchayaphon <56754@yupparaj.ac.th>
2026-06-25 08:59:39 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("My Digital Profile")
+
+st.header("แนะนำตัว")
+st.write("นางสาวพิชญาภรณ์ ทิพย์มณี")
+st.write("ชื่อเล่น แอปเปิ้ล")
+st.write("เลขที่ 27 ม.4/15")
+
+st.header("เป้าหมายและสิ่งที่อยากทำในอนาคต")
+st.write("เป้าหมาย: **อยากทำธุระกิจ ")
+st.write("สิ่งที่อยากเป้นในอนาคต: ceo")
