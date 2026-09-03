0da3e86c2e934613a1c5175ce21e5d4641b53d56
415-27-phitchayaphon <56754@yupparaj.ac.th>
2026-09-03 09:00:29 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -32,28 +32,28 @@ def show_result_dialog(ans1, ans2):
     u_ans3 = ans3.strip().lower()
     u_ans4 = ans4.strip().lower()
    
-  if u_ans1 == "apple":
+    if u_ans1 == "apple":
         st.success("✅ ข้อ 1: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
 
     
-  if u_ans2 == "fish":
+    if u_ans2 == "fish":
         st.success("✅ ข้อ 2: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
  
 
-  if u_ans3 == "Laptop":
+    if u_ans3 == "Laptop":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
 
-  if u_ans4 == "Blueberry":
+    if u_ans4 == "Blueberry":
           st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
